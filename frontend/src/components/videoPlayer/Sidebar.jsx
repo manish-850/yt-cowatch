47960c4ec502ea8d8037bb9ff8d2a3fdb481b1a7
Manish Kumar <manishkumar9576700@gmail.com>
2026-07-10 20:29:27 +0530

    feat: enhance sidebar styling and improve CSS variables for better UI consistency

--- frontend/src/components/videoPlayer/Sidebar.jsx
+++ frontend/src/components/videoPlayer/Sidebar.jsx
@@ -1,28 +1,21 @@
 import Chat from "../chat/Chat";
 import Users from "../chat/Users";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { MessageCircle, UserRound } from "lucide-react";
 
 export default function Sidebar() {
   return (
     <>
-      <div className="p-2">
+      <div className="sidebar">
         <Tabs defaultValue="chat" className="h-full">
-          <TabsList className="w-full">
-            <TabsTrigger className="cursor-pointer" value="chat">
-              <MessageCircle />
-              Chat
-            </TabsTrigger>
-            <TabsTrigger className="cursor-pointer" value="users">
-              <UserRound />
-              Users
-            </TabsTrigger>
+          <TabsList className="w-full sidebar-tabslist">
+            <TabsTrigger className="cursor-pointer sidebar-tabstrigger" value="chat">Chat</TabsTrigger>
+            <TabsTrigger className="cursor-pointer sidebar-tabstrigger" value="users">Users</TabsTrigger>
           </TabsList>
           <TabsContent value="chat" className="px-2">
-            <Chat />
+            <Chat/>
           </TabsContent>
           <TabsContent value="users">
-            <Users />
+            <Users/>
           </TabsContent>
         </Tabs>
       </div>
